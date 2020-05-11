6426018a482b98d71c9f15ae3a2a1176e6dfe69a
Ihor Breza <seniorigor@gmail.com>
2020-05-11 12:07:06 +0200

    Add domain model diagram with rails-erd

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Fix for rails-erd
+if Rails.env.development?
+  Zeitwerk::Loader.eager_load_all
+end
